be041b44d5ec954c4e645feea431bb1019be1b66
movistarsIphone <55772946+movistarsIphone@users.noreply.github.com>
2021-09-02 19:52:17 -0500

    Update PlayerInput.java

--- src/main/java/minsait/ttaa/datio/common/naming/PlayerInput.java
+++ src/main/java/minsait/ttaa/datio/common/naming/PlayerInput.java
@@ -3,6 +3,14 @@ package minsait.ttaa.datio.common.naming;
 public final class PlayerInput {
 
     public static Field shortName = new Field("short_name");
+    /**Inicio - MACH**/
+    public static Field longName = new Field("long_name");
+    public static Field age = new Field("age");
+    public static Field weight_kg = new Field("weight_kg");
+    public static Field nationality = new Field("nationality");
+    public static Field club_name = new Field("club_name");
+    public static Field potential = new Field("potential");
+    /**Fin - MACH**/
     public static Field overall = new Field("overall");
     public static Field teamPosition = new Field("team_position");
     public static Field heightCm = new Field("height_cm");
