d982a842ea6260fe63c0f56122d61a51d6b9e264
movistarsIphone <55772946+movistarsIphone@users.noreply.github.com>
2021-09-02 19:54:19 -0500

    Update Writer.java

--- src/main/java/minsait/ttaa/datio/engine/Writer.java
+++ src/main/java/minsait/ttaa/datio/engine/Writer.java
@@ -4,18 +4,32 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 
 import static minsait.ttaa.datio.common.Common.*;
+import static minsait.ttaa.datio.common.naming.PlayerInput.longName;
+import static minsait.ttaa.datio.common.naming.PlayerInput.shortName;
+import static minsait.ttaa.datio.common.naming.PlayerInput.age;
 import static minsait.ttaa.datio.common.naming.PlayerInput.teamPosition;
 import static org.apache.spark.sql.SaveMode.Overwrite;
 
 abstract class Writer {
 
     static void write(Dataset<Row> df) {
-        df
+        
+//    	df
+//        .coalesce(2)
+//        .write()
+//        .partitionBy(teamPosition.getName())
+//        .mode(Overwrite)
+//        .parquet(OUTPUT_PATH);
+    	
+    	/** Inicio - MACH **/
+    	df
                 .coalesce(2)
                 .write()
-                .partitionBy(teamPosition.getName())
-                .mode(Overwrite)
+        		.partitionBy(shortName.getName())
+        		.partitionBy(age.getName())
+        		.mode(Overwrite)
                 .parquet(OUTPUT_PATH);
+    	/** Fin - MACH **/
     }
 
 }
