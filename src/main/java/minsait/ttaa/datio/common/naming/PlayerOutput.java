560b28741f0c200022456d92a1f77ab89dbf08ba
movistarsIphone <55772946+movistarsIphone@users.noreply.github.com>
2021-09-02 19:52:42 -0500

    Update PlayerOutput.java

--- src/main/java/minsait/ttaa/datio/common/naming/PlayerOutput.java
+++ src/main/java/minsait/ttaa/datio/common/naming/PlayerOutput.java
@@ -3,5 +3,10 @@ package minsait.ttaa.datio.common.naming;
 public final class PlayerOutput {
 
     public static Field catHeightByPosition = new Field("cat_height_by_position");
-
+	/**Inicio - MACH**/
+	public static Field age_range = new Field("age_range");
+	public static Field rank_by_nationality_position = new Field("rank_by_nationality_position");
+	public static Field potential_vs_overall = new Field("potential_vs_overall");
+	/**Fin - MACH**/
+	
 }
